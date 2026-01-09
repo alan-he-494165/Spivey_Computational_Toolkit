98789b7e0f64d7db07f8a36ed3e548773f1ba20f
Alan-XH-Chem <alanxh.research@gmail.com>
2026-01-09 20:36:41 +0000

    update orca_xyz - function explanation and geometry calculation, also sorting

--- spivey_computational_toolkit/orca_py/orca_xyz.py
+++ spivey_computational_toolkit/orca_py/orca_xyz.py
@@ -1,6 +1,15 @@
+import numpy as np
+import math
+
 class atom:
     """
     Class to represent an atom in an XYZ file
+    PARAMETERS
+    ----------
+        atom_type (str): type of the atom (e.g., "C", "H", "O")
+        x (float): x-coordinate
+        y (float): y-coordinate
+        z (float): z-coordinate 
     """
     def __init__(self, atom_type:str, x, y, z):
         self.atom_type = atom_type
@@ -11,6 +20,19 @@ class atom:
 class xyz_molecule:
     """
     Read and visualize XYZ files from ORCA or similar computational chemistry software
+    PARAMETERS
+    ----------
+        comment (str): comment line from the XYZ file
+        atom_list (list): list of atom objects
+        energy (float): energy extracted from the comment line if available
+
+    METHODS
+    -------
+        from_xyz(filepath): class method to create an instance from an XYZ file
+        add_atom(atom_type, x, y, z): add an atom to the molecule
+        to_xyz(filepath): write the molecule to an XYZ file
+        get_distance(atom_index1, atom_index2): calculate distance between two atoms
+        get_bond_angle(atom_index1, atom_index2, atom_index3): calculate bond angle between three atoms
     """
     
     def __init__(self):
@@ -20,7 +42,15 @@ class xyz_molecule:
     
     @classmethod
     def from_xyz(cls,filepath):
-        """Parse XYZ file and extract data"""
+        """Parse XYZ file and extract data
+        PARAMETERS
+        ----------
+            filepath (str): path to the XYZ file
+
+        RETURNS
+        -------
+            instance (xyz_molecule): instance of the xyz_molecule class
+        """
         instance = cls()
         with open(filepath, 'r') as f:
             lines = [line.rstrip('\n') for line in f]
@@ -41,21 +71,83 @@ class xyz_molecule:
                 
             return instance
 
-    def add_atom(self, atom_type:str, x, y, z):
+    def add_atom(self, atom_type:str = "C", x = 0, y = 0, z = 0):
         """Add an atom to the current frame"""
         self.atom_list.append(atom(atom_type, x, y, z))
 
     def to_xyz(self, filepath):
-        """Write the molecule to an XYZ file"""
+        """Write the molecule to an XYZ file
+        PARAMETERS
+        ----------
+            filepath (str): path to the output XYZ file
+        """
         with open(filepath, 'w') as f:
             f.write(f"{len(self.atom_list)}\n")
             f.write(f"{self.comment}\n")
             for atom in self.atom_list:
                 f.write(f"{atom.atom_type}  {atom.x:.6f}    {atom.y:.6f}    {atom.z:.6f}\n")
 
+    def get_distance(self, atom_index1:int, atom_index2:int) -> float:
+        """Calculate the distance between two atoms in the molecule
+        PARAMETERS
+        ----------
+            atom_index1 (int): index of the first atom
+            atom_index2 (int): index of the second atom
+
+        RETURNS
+        -------
+            distance (float): distance between the two atoms
+        """
+        import math
+        atom1 = self.atom_list[atom_index1]
+        atom2 = self.atom_list[atom_index2]
+        dx = atom1.x - atom2.x
+        dy = atom1.y - atom2.y
+        dz = atom1.z - atom2.z
+        return math.sqrt(dx*dx + dy*dy + dz*dz)
+    
+    def get_bond_angle(self, atom_index1:int, atom_index2:int, atom_index3:int) -> float:
+        """
+        Calculate the bond angle between three atoms in the molecule
+        PARAMETERS
+        ----------
+            atom_index1 (int): index of the first atom
+            atom_index2 (int): index of the second atom (vertex)
+            atom_index3 (int): index of the third atom
+        
+        RETURNS
+        -------
+            angle (float): bond angle in degrees
+        """
+        
+        atom1 = self.atom_list[atom_index1]
+        atom2 = self.atom_list[atom_index2]
+        atom3 = self.atom_list[atom_index3]
+        
+        v1 = np.array([atom1.x - atom2.x, atom1.y - atom2.y, atom1.z - atom2.z])
+        v2 = np.array([atom3.x - atom2.x, atom3.y - atom2.y, atom3.z - atom2.z])
+
+        cos_angle = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+        angle = math.acos(cos_angle)
+        
+        return math.degrees(angle)
+    
+
 class mol_group:
     """
     Class to represent a group of molecules from an XYZ file
+    PARAMETERS
+    ----------
+        molecules (list): list of xyz_molecule objects
+        n_mol (int): number of molecules in the group
+        energy (list): list of energies for each molecule
+
+    METHODS
+    -------
+        load_trj(filepath): class method to load a trajectory from an XYZ file
+        load_allxyz(filepath): class method to load a trajectory from an allxyz file
+        to_trj(filepath): write the molecule group to an XYZ trajectory file
+        to_allxyz(filepath): write the molecule group to an allxyz file
     """
     def __init__(self):
         self.molecules = []
@@ -110,15 +202,24 @@ class mol_group:
         return instance
     
     def to_trj(self, filepath):
-        """Write the molecule group to an XYZ trajectory file"""
+        """Write the molecule group to an XYZ trajectory file
+        PARAMETERS
+        ----------
+            filepath (str): path to the output XYZ trajectory file
+        """
         with open(filepath, 'w') as f:
             for mol in self.molecules:
                 f.write(f"{len(mol.atom_list)}\n")
                 f.write(f"{mol.comment}\n")
                 for atom in mol.atom_list:
                     f.write(f"{atom.atom_type}  {atom.x:.6f}    {atom.y:.6f}    {atom.z:.6f}\n")
+
     def to_allxyz(self, filepath):
-        """Write the molecule group to an allxyz file"""
+        """Write the molecule group to an allxyz file
+        PARAMETERS
+        ----------
+            filepath (str): path to the output allxyz file
+        """
         with open(filepath, 'w') as f:
             for mol in self.molecules:
                 f.write(f"{len(mol.atom_list)}\n")
@@ -128,3 +229,27 @@ class mol_group:
                 f.write("\n")
                 if mol != self.molecules[-1]:
                     f.write(">\n")
+
+    def sort_by_energy(self, overwrite:bool = False):
+        """Sort the molecules in the group by their energy values
+        PARAMETERS
+        ----------
+            overwrite (bool): if True, overwrite the current order of molecules
+
+        RETURNS (if overwrite is False)
+        -------------------------------
+            index (list): list of indices that would sort the molecules by energy
+            energy (list): sorted list of energies
+        """
+        if len(self.energy) != self.n_mol:
+            raise ValueError("Energy list length does not match number of molecules.")
+        elif self.n_mol == 0:
+            raise ValueError("No molecules to sort.")
+        if overwrite:
+            sorted_molecules = [mol for _, mol in sorted(zip(self.energy, self.molecules), key=lambda x: x[0])]
+            self.molecules = sorted_molecules
+            self.energy.sort()
+            return
+        index, energy = zip(*sorted(enumerate(self.energy), key=lambda x: x[1]))
+        return index, energy
+    
